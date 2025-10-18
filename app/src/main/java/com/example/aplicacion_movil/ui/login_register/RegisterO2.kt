5c407c66ba74f67868cc4fea4c1d07b972c7daac
Alessapac74 <apachari@est.unap.edu.pe>
2025-10-18 15:31:12 -0500

    login

--- app/src/main/java/com/example/aplicacion_movil/ui/login_register/RegisterO2.kt
+++ app/src/main/java/com/example/aplicacion_movil/ui/login_register/RegisterO2.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import com.example.aplicacion_movil.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -24,7 +22,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.MotionScene
 
 @Preview(showSystemUi = true)
 @Composable
@@ -39,7 +36,7 @@ fun RegisterS() {
                 .fillMaxSize()
                 .padding(24.dp)
         ) {
-            val (titleRef, googleButtonRef, emailButtonRef, emailTextRef,logoRef) = createRefs()
+            val (titleRef, googleButtonRef, emailButtonRef, logoRef) = createRefs()
 
             Text(
                 text = "Registro gratuito",
